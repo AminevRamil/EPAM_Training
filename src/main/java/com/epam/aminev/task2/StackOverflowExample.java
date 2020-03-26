0d8baba1c5e5e1f2a4500e094195ce9a5c9fd5e6
StarBun1379 <41346424+StarBun1379@users.noreply.github.com>
2020-03-26 15:15:53 +0400

    Add javadoc

--- src/main/java/com/epam/aminev/task2/StackOverflowExample.java
+++ src/main/java/com/epam/aminev/task2/StackOverflowExample.java
@@ -2,8 +2,19 @@ package com.epam.aminev.task2;
 
 import lombok.extern.slf4j.Slf4j;
 
+/**
+ * Simple class to demonstrate StackOverflowError
+ * That error will causes by calling unregulated
+ * recursive method that overflow the stack
+ *
+ * @author Aminev Ramil
+ */
 @Slf4j
 public class StackOverflowExample {
+    /**
+     * Entry point in demo
+     * @param args - input argument. Not used
+     */
     public static void main(String[] args) {
         try {
             long maxDeepLevel = recursive(0L);
@@ -13,6 +24,12 @@ public class StackOverflowExample {
         }
     }
 
+    /**
+     * Recursive function
+     * @param level of depth of recursion
+     * @return recursive call
+     * @throws StackOverflowError when stack will overflowed
+     */
     static long recursive(long level) throws StackOverflowError {
         return recursive(level + 1);
     }
--- src/main/java/com/epam/aminev/task3/Main.java
+++ src/main/java/com/epam/aminev/task3/Main.java
@@ -2,8 +2,15 @@ package com.epam.aminev.task3;
 
 import lombok.extern.slf4j.Slf4j;
 
+/**
+ * Main class that demonstrate understanding of how class loaders work
+ */
 @Slf4j
 public class Main {
+    /**
+     * Entry point in demo that was made during lesson 7
+     * @param args - input arguments. Not used
+     */
     public static void main(String[] args) {
         CustomClassLoader classLoader = new CustomClassLoader("C:\\Users\\Optim\\Desktop\\myClasses\\");
         Class<?> clazz;
